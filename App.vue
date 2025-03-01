b34ffbc178f0afcf416063ab885bc903ecb51201
Jack Chen <165772885+Gundamten@users.noreply.github.com>
2025-03-01 21:02:42 +0800

    1.模板语法

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h3>模板语法</h3>
+  <p>{{ msg }}</p>
+  <p>Hello World</p>
+  <p>{{ number + 1 }}</p>
+  <p>{{ ok ? 'Yes' : 'No' }}</p>
+  <p>{{ message.split('').reverse().join('') }}</p>
+  <p>{{ rawHtml }}</p>   <!-- 这一步不能只会显示引号内的所有内容 -->
+  <p v-html = "rawHtml"></p>  <!--这样才能识别链接-->
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      msg:"神奇的语法",
+      number:10,
+      ok:true,
+      message:"大家好",
+      rawHtml:"<a href = 'https://itbaizhan.com'>百战程序员</a>"
+    }
+  }
+}
+</script>
+
+
+
