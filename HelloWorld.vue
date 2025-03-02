c308a71b49bdb989b26eb5b318a43f7cf0d73aef
Jack Chen <165772885+Gundamten@users.noreply.github.com>
2025-03-02 20:44:03 +0800

    02属性绑定

---
+++ HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-bind:id="dynamicID" v-bind:class="dynamicClass" v-bind:title="dynamicTitle">测试</div>
+  <button :disabled="isButtonDisabled">Button</button>
+  <div v-bind="objectOfAttrs">测试</div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      dynamicClass:"active",
+      dynamicID:"appid",
+      dynamicTitle:"hahaha",
+      isButtonDisabled:false,
+      objectOfAttrs:{
+        dynamicClass:"active",
+        dynamicID:"appid"
+      }
+    }
+  }
+}
+</script>
+
