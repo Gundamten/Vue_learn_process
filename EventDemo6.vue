fe847160828ceb6ffacaeec46b48bf59e9ac4045
Jack Chen <165772885+Gundamten@users.noreply.github.com>
2025-03-05 16:21:09 +0800

    08事件修饰符

---
+++ EventDemo6.vue
@@ -0,0 +1,32 @@
+<template>
+    <h3>事件修饰符</h3>
+    <a @click.prevent="clickHandle" href="https://baidu.com">百战程序员</a>    <!--阻止的是跳转的动作-->
+    <div @click="clickDiv">
+        <p @click.stop="clickP">测试冒泡</p>
+    </div>
+</template>
+<script>
+
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        clickHandle(e){
+            // 阻止默认事件
+            // e.preventDefault();
+            console.log("点击了");
+        },
+        clickDiv(){
+            console.log("DIV");            
+        },
+        clickP(e){
+            // e.stopPropagation();
+            console.log("P");            
+        }
+    }
+}
+
+</script>
