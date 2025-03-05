d02069aa0b6acc4dfd9a543d8f0949aa52b1534f
Jack Chen <165772885+Gundamten@users.noreply.github.com>
2025-03-05 17:05:10 +0800

    10计算属性

---
+++ ComputedDemo.vue
@@ -0,0 +1,32 @@
+<template>
+    <h3>{{ itbaizhan.name }}</h3>
+    <!-- <p>{{ itbaizhan.content.length > 0 ? 'Yes' : 'No' }}</p>    官方不推荐将运算放在这里 -->
+     <p>{{ itbaizhanContent }}</p>    <!--计算属性不需要加括号-->
+     <p>{{ itbaizhanContents() }}</p>     <!--函数需要加括号-->
+</template>
+<script>
+
+export default {
+    data(){
+        return{
+            itbaizhan:{
+                name:"百战程序员",
+                content:["前端","Java","python"]
+            }
+        }
+    },
+    // 计算属性
+    computed:{
+        itbaizhanContent(){
+            return this.itbaizhan.content.length > 0 ? 'Yes' : 'No'
+        }
+    },
+    // 或者将其放在methods方法当中
+    methods:{
+        itbaizhanContents(){
+            return this.itbaizhan.content.length > 0 ? 'Yes' : 'No'
+        }      
+    }
+}
+
+</script>
