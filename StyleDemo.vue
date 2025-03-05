eead39272e467643ea374b1f37c3e77dde319523
Jack Chen <165772885+Gundamten@users.noreply.github.com>
2025-03-05 21:39:47 +0800

    Style绑定

---
+++ StyleDemo.vue
@@ -0,0 +1,21 @@
+<template>
+    <p :style="{color:activeColor,fontSize:fontSize +'px'}">Style绑定1</p>
+    <p :style="styleObject">Style绑定2</p>
+    <p :style="[styleObject]">Style绑定3</p>
+</template>
+<script>
+
+export default {
+    data(){
+        return{
+            activeColor:'green',
+            fontSize:30,    //单位px不能直接放在30后面
+            styleObject:{
+                color:"red",
+                fontSize:"30px"
+            }
+        }
+    }
+}
+
+</script>
