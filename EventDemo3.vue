b2389999708f3ff1002bc9ad279d376190b3268d
Jack Chen <165772885+Gundamten@users.noreply.github.com>
2025-03-05 15:59:37 +0800

    07事件传参

---
+++ EventDemo3.vue
@@ -0,0 +1,26 @@
+<template>
+    <h3>内联事件处理器</h3>
+    <button @click="addCount">Add</button>    <!--@符号是由v-on:简化而成的-->
+    <p>{{ count }}</p>
+</template>
+<script>
+
+export default{
+    data(){
+        return{
+            count:0
+        }
+    },
+    // 所有的方法或者函数都放在这里
+    methods:{
+        // event对象
+        addCount(e){
+            // 读取到data里面的数据的方案：this.count
+            // Vue中的event对象，就是原生JS的Event对象
+            e.target.innerHTML = "Add" + this.count;
+            this.count++
+        }
+    }
+}
+
+</script>
---
+++ EventDemo4.vue
@@ -0,0 +1,27 @@
+<template>
+    <h3>内联事件处理器</h3>
+    <button @click="addCount('hello')">Add</button>    <!--@符号是由v-on:简化而成的-->
+    <p>{{ count }}</p>
+</template>
+<script>
+
+export default{
+    data(){
+        return{
+            count:0
+        }
+    },
+    // 所有的方法或者函数都放在这里
+    methods:{
+        // event对象
+        addCount(msg){
+            // 读取到data里面的数据的方案：this.count
+            // Vue中的event对象，就是原生JS的Event对象
+            // e.target.innerHTML = "Add" + this.count;
+            console.log(msg);
+            this.count++
+        }
+    }
+}
+
+</script>
