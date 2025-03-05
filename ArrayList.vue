3b5480e4c7977ca5d2c753f568cba396a5c89c02
Jack Chen <165772885+Gundamten@users.noreply.github.com>
2025-03-05 16:45:06 +0800

    09数组变化侦测

---
+++ ArrayList.vue
@@ -0,0 +1,41 @@
+<template>
+    <h3>数组变化侦听</h3>
+    <button @click="addListHandle">添加数据</button>
+    <ul>
+        <li v-for="(item,index) of names" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="addArrayHandle">合并数组</button>
+    <h3>数组1</h3>
+    <p v-for="(item,index) of nums1" :key="index">{{ item }}</p>
+    <h3>数组2</h3>
+    <p v-for="(item,index) of nums2" :key="index">{{ item }}</p>
+</template>
+<script>
+
+export default {
+    data(){
+        return{
+            names:["iwen","ime","frank"],
+            nums1:[1,2,3,4,5],
+            nums2:[6,7,8,9,10]
+        }
+    },
+    methods:{
+        addListHandle(){
+            // 引起UI自动更新
+            // this.names.push("sakura")
+            // 不会引起UI自动更新
+            this.names.concat(["sakura"]);
+            // console.log(this.names.concat(["sakura"]));
+            // 将值赋给this.names
+            this.names = this.names.concat(["sakura"]);
+        },
+
+        addArrayHandle(){
+            // this.nums1.concat(this.nums2);
+            this.nums1 = this.nums1.concat(this.nums2);
+        }
+    }
+}
+
+</script>
