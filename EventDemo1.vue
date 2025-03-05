21fac0a8382a1c91881b547526eb776a996d178b
Jack Chen <165772885+Gundamten@users.noreply.github.com>
2025-03-05 15:36:02 +0800

    06事件处理

---
+++ EventDemo1.vue
@@ -0,0 +1,16 @@
+<template>
+    <h3>内联事件处理器</h3>
+    <button @click="count++">Add</button>    <!--@符号是由v-on:简化而成的-->
+    <p>{{ count }}</p>
+</template>
+<script>
+
+export default{
+    data(){
+        return{
+            count:0
+        }
+    }
+}
+
+</script>
